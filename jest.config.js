8a31d446bc2e6d6ffb25cea62fc276ec6d7b1913
WilhelmYakunin <v.jakunin@inbox.ru>
2022-11-18 11:58:03 +0300

    ref: move test to one one folder

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     testEnvironment: "jsdom",
     testPathIgnorePatterns: ["/lib/", "/node_modules/"],
     modulePaths: ['<rootDir>/src', '<rootDir>/node_modules'],
-    testRegex: '(/src/.*\\.(test|spec))\\.(ts|tsx|js)$',
+    testRegex: '(/src/__test__.*\\.(test|spec))\\.(ts|tsx|js)$',
     moduleNameMapper: {
         '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
