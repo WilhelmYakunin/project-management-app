74f2859b60ddbac124d7e8752e0d42148bb955ae
Kate Hubar <70864294+KateHubar@users.noreply.github.com>
2022-11-25 14:42:28 +0300

    feat: add team description

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -1,7 +1,58 @@
-import  style from './Team.module.css';
+import style from './Team.module.css';
 
 export const Team = () => {
-  return(
-  <div  className={style.text}></div>
-  )
-}
+  return (
+    <div className={style.team_wrapper}>
+      <p className={style.team_title}>Team</p>
+      <div className={style.team_string}>
+      <ul className={style.team_list}>
+        <li>
+          <a href="https://github.com/WilhelmYakunin" target="_blank" rel="noreferrer">
+            <div className={style.team_ava}></div>
+          </a>
+        </li>
+        <li className={style.team_text}>
+          <a href="https://github.com/WilhelmYakunin" target="_blank" rel="noreferrer">
+            WilhelmYakunin
+          </a>
+        </li>
+        <li>
+          <p>Team Lead, Frontend developer</p>
+        </li>
+      </ul>
+
+      <ul className={style.team_list}>
+        <li>
+          <a href="https://github.com/fraterpavlo" target="_blank" rel="noreferrer">
+            <div className={style.team_ava}></div>
+          </a>
+        </li>
+        <li className={style.team_text}>
+          <a href="https://github.com/fraterpavlo" target="_blank" rel="noreferrer">
+          fraterpavlo
+          </a>
+        </li>
+        <li>
+          <p>Frontend developer</p>
+        </li>
+      </ul>
+      
+      <ul className={style.team_list}>
+        <li>
+          <a href="https://github.com/KateHubar" target="_blank" rel="noreferrer">
+            <div className={style.team_ava}></div>
+          </a>
+        </li>
+        <li className={style.team_text}>
+          <a href="https://github.com/KateHubar" target="_blank" rel="noreferrer">
+          KateHubar
+          </a>
+        </li>
+        <li>
+          <p>Frontend developer</p>
+        </li>
+      </ul>
+      </div>
+    </div>
+  );
+};
