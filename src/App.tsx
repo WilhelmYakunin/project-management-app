c5e5cff47b848db73d988f0bb51051bcbb6391fe
Vitalij Yakunin <vit.yakunin@gmail.com>
2022-11-28 16:04:57 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import LocalesSelect from './features/locales/localesSelect';
 
 function App() {
   return (
     <div className="App">
-      <LocalesSelect />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
