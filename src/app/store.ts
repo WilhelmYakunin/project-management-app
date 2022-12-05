fca6a5f9045075b7209a340e33b9ffac1b98b528
WilhelmYakunin <v.jakunin@inbox.ru>
2022-12-05 11:11:32 +0300

    resolve conflicts develop and modals

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,24 +2,18 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import userReducer from './reducers/user';
 import localeReducer from '../features/locales/localeSlice';
-<<<<<<< HEAD
 import modalReducer from '../features/modals/modalsSlice';
-=======
 import boardsPageReducer from './reducers/boards-page-slice';
 import specifiedBoardPageReducer from './reducers/specified-board-page-slice';
->>>>>>> develop
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     user: userReducer,
     locale: localeReducer,
-<<<<<<< HEAD
     modal: modalReducer,
-=======
     boardsPage: boardsPageReducer,
     specifiedBoardPage: specifiedBoardPageReducer,
->>>>>>> develop
   },
 });
 
